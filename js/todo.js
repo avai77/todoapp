058f1a4cd82847000f246ee13b49286f0b5bdadb
avai77 <49594498+avai77@users.noreply.github.com>
2019-09-20 07:29:27 +0600

    Add files via upload

---
+++ js/todo.js
@@ -0,0 +1,98 @@
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+const CHECK = "fa-check-circle";
+const UNCHECK = "far fa-check-square";
+const REMOVE = "remove";
+
+let LIST, id;
+let data = localStorage.getItem("TODO");
+
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length; 
+    loadList(LIST); 
+}else{  
+    LIST = [];
+    id = 0;
+}
+function loadList(array){
+    array.forEach(function(item){
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
+
+function addToDo(toDo, id, done, trash){
+    if(trash){ return; }
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? REMOVE : "";
+    const item = 
+    `<li class="item">
+        <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+        <p class="text ${LINE}">${toDo}</p>
+        <i class="fas fa-biohazard" job="delete" id="${id}"></i>
+    </li>`;
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
+} 
+
+document.addEventListener("keyup",function(even){
+    if(event.keyCode == 13){
+        const toDo = input.value;
+        if(toDo){
+            addToDo(toDo, id, false, false);
+            LIST.push({
+                name : toDo,
+                id : id,
+                done : false,
+                trash : false
+            });
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+            id++;
+        }
+        input.value = "";
+    }
+});
+function completeToDo(element){
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(REMOVE);
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+function removeToDo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    LIST[element.id].trash = true;
+}
+
+list.addEventListener("click", function(event){
+    const element = event.target; 
+    const elementJob = element.attributes.job.value; 
+    
+    if(elementJob == "complete"){
+        completeToDo(element);
+    }else if(elementJob == "delete"){
+        removeToDo(element);
+    }
+    
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
